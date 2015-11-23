da42006d3e8f73070fa3d5c7560236f73600fa8f
steph-harris <steph.harris21@gmail.com>
2015-11-23 02:40:02 -0500

    added add function to list, and linked checkboxes to checked class in css

--- scripts/table.js
+++ scripts/table.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {  
-  debugger;
+  
   $(document).on("click","table .btn-danger", function() {
     $(this).parents("tr").remove();
   });
-  $(".btn-success").on("click", function() {
-    $(this).parents("tr").remove();
+
+  $(".btn-success").on("click", function(e) {
+    
+    e.preventDefault();   
+    var forminput = $("#formtext").val ();
+    var addRow = $("<tr>");
+    var rowTd1 = $("<td>").append(forminput);
+    var checkbox = $("<input>", {
+      type:"checkbox"
+    });
+    var rowTd2 = $("<td>").append(checkbox);
+    var delButton = $("<button>").addClass("btn btn-danger").append("Delete");
+    var rowTd3 = $("<td>").append(delButton);    
+    addRow.append(rowTd1);
+    addRow.append(rowTd2);    
+    addRow.append(rowTd3);
+    $("tbody").append(addRow);   
   });
+
+  // $(document).on("click","table input",function(){
+  //   // debugger;
+  //   $("tr:first-child").addClass("checked");
+  // } )
 });
+// empty the form-control after the row is added to table
