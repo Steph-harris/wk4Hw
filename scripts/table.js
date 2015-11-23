2931cfece12650574e188f1a87f9619c5fc7144a
steph-harris <steph.harris21@gmail.com>
2015-11-23 00:18:12 -0500

    added checkboxes to todo list

--- scripts/table.js
+++ scripts/table.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
   $(document).on("click","table .btn-danger", function() {
     $(this).parents("tr").remove();
   });
+  $(".btn-success").on("click", function() {
+    $(this).parents("tr").remove();
+  });
 });
